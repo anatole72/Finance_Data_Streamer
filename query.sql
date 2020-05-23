59057f2a949e25e34c933fd3b226e4cb5b185d3e
=tbenthomas <tbenthomas@yahoo.com>
2020-05-23 14:21:18 -0400

    data + query results

--- query.sql
+++ query.sql
@@ -1,4 +1,4 @@
-SELECT name,MAX(high) as high,substr(ts,1,11) as date,substr(ts,11,3) as hour
+SELECT name,MAX(high) AS high,substr(ts,1,11) AS date,substr(ts,11,3) AS hour
 FROM stream_stock_prices
 GROUP BY name,substr(ts,1,11),substr(ts,11,3)
-ORDER BY name,Hour;
+ORDER BY name,hour;
