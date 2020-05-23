9d8bb741280d3149a777a15404cb0895cc5637a0
=tbenthomas <tbenthomas@yahoo.com>
2020-05-23 17:47:23 -0400

    athena query and results

--- query.sql
+++ query.sql
@@ -1,4 +1,4 @@
-SELECT name,MAX(high) high,date(CAST(ts AS timestamp)) date,hour(CAST(ts AS timestamp))-4 hour
+SELECT name,MAX(high) high,MAX(date_add('hour',-4,CAST(ts AS timestamp))) AS datetime,hour(CAST(ts AS timestamp))-4 hour
 FROM stream_stock_prices
-GROUP BY name,date(CAST(ts AS timestamp)),hour(cast(ts as timestamp))
+GROUP BY name,hour(cast(ts as timestamp))
 ORDER BY name,hour;
