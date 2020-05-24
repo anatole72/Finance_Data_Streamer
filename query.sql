124e833eebb93fe56b7c2fe1822b0c770265a30e
=tbenthomas <tbenthomas@yahoo.com>
2020-05-23 20:26:29 -0400

    Query and results

--- query.sql
+++ query.sql
@@ -1,4 +1,10 @@
-SELECT name,MAX(high) high,MAX(date_add('hour',-4,CAST(ts AS timestamp))) AS datetime,hour(CAST(ts AS timestamp))-4 hour
+SELECT agg.name name,agg.high high,date_add('hour',-4,CAST(ts AS timestamp)) datetime,agg.hour hour
+FROM stream_stock_prices t
+JOIN
+(
+SELECT name,MAX(high) high,hour(CAST(ts AS timestamp))-4 hour
 FROM stream_stock_prices
 GROUP BY name,hour(cast(ts as timestamp))
-ORDER BY name,hour;
+) agg
+ON t.name=agg.name AND t.high=agg.high AND hour(CAST(ts AS timestamp))-4=hour
+ORDER BY name,hour,datetime;
